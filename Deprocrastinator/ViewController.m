9a71889398664cf9ac30c28e60237323c1d28ca5
Ben Whitley <blw305@gmail.com>
2015-03-16 14:10:49 -0500

    can't remember progress made

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate>
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property NSMutableArray *toDoListArray;
+@property (weak, nonatomic) IBOutlet UITableView *toDoListTableView;
 
 @end
 
@@ -16,18 +19,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.toDoListArray = [NSMutableArray arrayWithObjects: @"Laundry", @"Homework", @"Call mom", @"cook dinner", nil];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 4;
+    return self.toDoListArray.count;
 }
--(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
-    return cell;
-}
+
 - (IBAction)onAddButtonPressed:(UIBarButtonItem *)sender {
-    
+    NSString *text = self.textField.text;
+    [self.toDoListArray addObject:text];
+    [self.toDoListTableView reloadData];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.textField resignFirstResponder];
+    return YES;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.toDoListArray objectAtIndex:indexPath.row]];
+    return cell;
 }
 
 
